40bc3ae4d525a97fcb19813cd4dcf32dd4d4f28a
Diana Pulatova <dianapulatovaa@gmail.com>
2021-01-28 09:11:40 -0500

    added ans for q2

--- ctc/q2.go
+++ ctc/q2.go
@@ -19,3 +19,13 @@ for _, thisCase := range cases {
 		t.Errorf("reverse: input %q, expect %q, but got %q\n", thisCase.input, thisCase.expect, result)
 	}
 }
+
+// function to reverse a C style string, by returning a new reversed string
+func reverse(str string) string {
+	length := len(str)
+	revArr := []byte(str)
+	for i := length / 2; i >= 0; i-- {
+		revArr[i], revArr[length-i-1] = revArr[length-i-1], revArr[i]
+	}
+	return string(revArr)
+}
