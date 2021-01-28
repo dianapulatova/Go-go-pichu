b60a3b768a2e44fe55f610de55498b93173189f8
Diana Pulatova <dianapulatovaa@gmail.com>
2021-01-28 09:21:54 -0500

    modified question 2

--- ctc/q2.go
+++ ctc/q2.go
@@ -1,3 +1,16 @@
+
+// function to reverse a C style string, by returning a new reversed string
+func reverse(str string) string {
+	length := len(str)
+	revArr := []byte(str)
+	for i := length / 2; i >= 0; i-- {
+		revArr[i], revArr[length-i-1] = revArr[length-i-1], revArr[i]
+	}
+	return string(revArr)
+}
+
+
+
 import (
 	"testing"
 )
@@ -18,14 +31,4 @@ for _, thisCase := range cases {
 	if result != thisCase.expect {
 		t.Errorf("reverse: input %q, expect %q, but got %q\n", thisCase.input, thisCase.expect, result)
 	}
-}
-
-// function to reverse a C style string, by returning a new reversed string
-func reverse(str string) string {
-	length := len(str)
-	revArr := []byte(str)
-	for i := length / 2; i >= 0; i-- {
-		revArr[i], revArr[length-i-1] = revArr[length-i-1], revArr[i]
-	}
-	return string(revArr)
 }
