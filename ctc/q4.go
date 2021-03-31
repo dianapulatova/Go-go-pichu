48afd6135f2fc888318c2a78bc1776195ce71ed3
Diana Pulatova <dianapulatovaa@gmail.com>
2021-03-31 13:29:10 -0400

    added space counter

--- ctc/q4.go
+++ ctc/q4.go
@@ -1,3 +1,13 @@
 import (
 	"testing"
 )
+func countSpaces(str string) int {
+	charsArr := []rune(str)
+	countSpaces := 0
+	for i := 0; i < len(str); i++ {
+		if charsArr[i] == rune(' '){
+			countSpaces++
+		}
+	}
+	
+}
